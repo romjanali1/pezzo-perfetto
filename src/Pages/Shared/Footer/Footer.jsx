07f23c4371058817fc49e33be541bffa8674fed4
romjanali1 <mdromgana@gmail.com>
2023-11-13 20:46:23 +0600

    footer socil icon add

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-
+import { FaFacebookF, FaLinkedinIn, FaWhatsappSquare } from "react-icons/fa";
 
 const Footer = () => {
     return (
@@ -6,21 +6,22 @@ const Footer = () => {
         <footer className="footer footer-center p-10 bg-neutral text-white grid grid-cols-1 lg:grid-cols-2">
   <nav className="">
     <header className="footer-title">CONTACT US</header> 
-    <a className="link link-hover">About us</a>
-    <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Press kit</a>
+    <p className="link link-hover">Natore, Bangladesh</p>
+    <p className="link link-hover">webdeveloperromjanali@gmail.com</p>
   </nav> 
   <nav>
     <header className="footer-title">Follow US</header> 
-    <a className="link link-hover">Terms of use</a>
-    <a className="link link-hover">Privacy policy</a>
-    <a className="link link-hover">Cookie policy</a>
+    <p>Join us on social media</p>
+    <div className="flex space-x-4">
+    <a href="https://www.facebook.com/mnhRomjanAli"><FaFacebookF size = '35px'/></a>
+    <a href="https://www.linkedin.com/in/romjan-ali-b3b11627a/"><FaLinkedinIn size = '35px'/></a>
+    <a href="https://wa.link/piw7fb"><FaWhatsappSquare size = '35px'/></a>
+    </div>
   </nav>
 </footer>
 <footer className="footer footer-center p-4 bg-base-300 text-base-content">
   <aside>
-    <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+    <p>Copyright © 2023 - All right reserved.</p>
   </aside>
 </footer>
 
