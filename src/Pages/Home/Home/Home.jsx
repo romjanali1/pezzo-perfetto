fd6aee80ca5b0ea785870cc557aa835a89c60d14
romjanali1 <mdromgana@gmail.com>
2023-12-22 20:02:19 +0600

    slider add

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../Banner/Banner";
+import Categery from "../Categery/Categery";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Categery></Categery>
         </div>
     );
 };
