b01c294f6f9e2df0e0ba6cc6bd1022c782fc6854
romjanali1 <mdromgana@gmail.com>
2024-01-09 19:22:44 +0600

    catagery section add

--- src/Pages/Home/Categery/Categery.jsx
+++ src/Pages/Home/Categery/Categery.jsx
@@ -10,10 +10,16 @@ import slide2 from '../../../assets/home/slide2.jpg';
 import slide3 from '../../../assets/home/slide3.jpg';
 import slide4 from '../../../assets/home/slide4.jpg';
 import slide5 from '../../../assets/home/slide5.jpg';
+import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 
 const Categery = () => {
   return (
-    <Swiper
+    <section>
+      <SectionTitle
+      subHeading={"---From 11:00am to 10:00pm---"}
+      heading={"ORDER ONLINE"}
+      ></SectionTitle>
+          <Swiper
       slidesPerView={4}
       spaceBetween={30}
       centeredSlides={true}
@@ -24,21 +30,23 @@ const Categery = () => {
       className="mySwiper mb-20"
     >
       <SwiperSlide><img src={slide1} alt="" />
-      <h3 className="text-4xl uppercase text-center -mt-16 text-white">Salads</h3>
+      <h3 className="text-4xl uppercase text-center -mt-24 text-white">Salads</h3>
       </SwiperSlide>
       <SwiperSlide><img src={slide2} alt="" />
-      <h3 className="text-4xl uppercase text-center -mt-16 text-white">desserts </h3>
+      <h3 className="text-4xl uppercase text-center -mt-24 text-white">desserts </h3>
       </SwiperSlide>
       <SwiperSlide><img src={slide3} alt="" />
-      <h3 className="text-4xl uppercase text-center -mt-16 text-white"> Soups </h3>
+      <h3 className="text-4xl uppercase text-center -mt-24 text-white"> Soups </h3>
       </SwiperSlide>
       <SwiperSlide><img src={slide4} alt="" />
-      <h3 className="text-4xl uppercase text-center -mt-16 text-white">cake</h3>
+      <h3 className="text-4xl uppercase text-center -mt-24 text-white">cake</h3>
       </SwiperSlide>
       <SwiperSlide><img src={slide5} alt="" />
-      <h3 className="text-4xl uppercase text-center -mt-16 text-white">Salads</h3>
+      <h3 className="text-4xl uppercase text-center -mt-24 text-white">Salads</h3>
       </SwiperSlide>
     </Swiper>
+    </section>
+
   );
 };
 
--- src/Pages/Shared/SectionTitle/SectionTitle.jsx
+++ src/Pages/Shared/SectionTitle/SectionTitle.jsx
@@ -1,7 +1,9 @@
 
-const SectionTitle = () => {
+const SectionTitle = ({heading, subHeading}) => {
     return (
-        <div>
+        <div className="mx-auto text-center md:w-3/12 my-6">
+            <p className="text-yellow-500">{subHeading}</p>
+            <h3 className="text-4xl uppercase border-y-4 py-4 mb-2">{heading}</h3>
             
         </div>
     );
